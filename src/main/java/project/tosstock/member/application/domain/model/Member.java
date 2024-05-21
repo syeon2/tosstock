3d79a55155cc27395fa3a0f97d0a70294c5a0314
SUYEON KIM <waterkite94@gmail.com>
2024-05-21 23:56:27 +0900

    [FEAT] Add member domain and entity
    - Add: Member Domain class
    - Add: Member Entity class with packaged password class

---
+++ src/main/java/project/tosstock/member/application/domain/model/Member.java
@@ -0,0 +1,34 @@
+package project.tosstock.member.application.domain.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class Member {
+
+	private Long id;
+
+	private String username;
+
+	private String email;
+
+	private String password;
+
+	private String phoneNumber;
+
+	private String introduce;
+
+	private String profileImageUrl;
+
+	@Builder
+	private Member(Long id, String username, String email, String password, String phoneNumber, String introduce,
+		String profileImageUrl) {
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+		this.introduce = introduce;
+		this.profileImageUrl = profileImageUrl;
+	}
+}
