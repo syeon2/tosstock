518ea41ab9c9ab97e1e18a23b1e9b3e07552a9ee
SUYEON KIM <waterkite94@gmail.com>
2024-05-22 10:37:00 +0900

    [TEST] Add tests for join member in the service layer

--- src/test/java/project/tosstock/member/adapter/out/MemberPersistenceAdapterTest.java
+++ src/test/java/project/tosstock/member/adapter/out/MemberPersistenceAdapterTest.java
@@ -31,14 +31,7 @@ class MemberPersistenceAdapterTest extends IntegrationTestSupport {
 	@DisplayName(value = "회원 도메인을 parameter로 받아 Repository에 저장합니다.")
 	void save_member() {
 		// given
-		Member member = Member.builder()
-			.username("suyeon")
-			.email("waterkite94@gmail.com")
-			.password("12345678")
-			.phoneNumber("01000001111")
-			.introduce("반갑습니다.")
-			.profileImageUrl("www.naver.com")
-			.build();
+		Member member = createMember("waterkite94@gmail.com", "01000001111");
 
 		// when
 		Long savedMemberId = memberPersistenceAdapter.saveMember(member);
@@ -47,5 +40,73 @@ class MemberPersistenceAdapterTest extends IntegrationTestSupport {
 		Optional<MemberEntity> findMemberOptional = memberRepository.findById(savedMemberId);
 		assertThat(findMemberOptional).isPresent()
 			.hasValueSatisfying(m -> assertThat(m.getId()).isEqualTo(savedMemberId));
+	}
+
+	@Test
+	@DisplayName(value = "저장소에 가입하고자 하는 이메일과 중복된 값이 있는지 확인합니다. (값이 없을 경우)")
+	void is_duplicated_Email_Non() {
+		// given
+		String email = "waterkite94@gmail.com";
+
+		// when
+		boolean isDuplicated = memberPersistenceAdapter.isDuplicatedEmail(email);
+
+		// then
+		assertThat(isDuplicated).isFalse();
+	}
+
+	@Test
+	@DisplayName(value = "저장소에 가입하고자 하는 이메일과 중복된 값이 있는지 확인합니다. (값이 있는 경우)")
+	void is_duplicated_Email() {
+		// given
+		String email = "waterkite94@gmail.com";
+		Member member = createMember(email, "01011112222");
+		memberPersistenceAdapter.saveMember(member);
+
+		// when
+		boolean isDuplicated = memberPersistenceAdapter.isDuplicatedEmail(email);
+
+		// then
+		assertThat(isDuplicated).isTrue();
+	}
+
+	@Test
+	@DisplayName(value = "저장소에 가입하고자 하는 전화번호와 중복된 값이 있는지 확인합니다. (값이 있는 경우)")
+	void is_duplicated_PhoneNumber() {
+		// given
+		String phoneNumber = "01011112222";
+
+		Member member = createMember("waterkite94@gmail.com", phoneNumber);
+		memberPersistenceAdapter.saveMember(member);
+
+		// when
+		boolean isExist = memberPersistenceAdapter.isExistPhoneNumber(phoneNumber);
+
+		// then
+		assertThat(isExist).isTrue();
+	}
+
+	@Test
+	@DisplayName(value = "저장소에 가입하고자 하는 전화번호와 중복된 값이 있는지 확인합니다. (값이 없는 경우)")
+	void is_duplicated_PhoneNumber_Non() {
+		// given
+		String phoneNumber = "01011112222";
+
+		// when
+		boolean isExist = memberPersistenceAdapter.isExistPhoneNumber(phoneNumber);
+
+		// then
+		assertThat(isExist).isFalse();
+	}
+
+	private Member createMember(String email, String phoneNumber) {
+		return Member.builder()
+			.username("suyeon")
+			.email(email)
+			.password("12345678")
+			.phoneNumber(phoneNumber)
+			.introduce("반갑습니다.")
+			.profileImageUrl("www.naver.com")
+			.build();
 	}
 }
