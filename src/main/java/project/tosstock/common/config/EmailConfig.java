899e09828b8104027c767b5c90740eb0bc819823
SUYEON KIM <waterkite94@gmail.com>
2024-05-23 12:10:39 +0900

    [FEAT] add email config and related exception

---
+++ src/main/java/project/tosstock/common/config/EmailConfig.java
@@ -0,0 +1,56 @@
+package project.tosstock.common.config;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+
+	@Value("${spring.mail.host}")
+	private String host;
+
+	@Value("${spring.mail.port}")
+	private Integer port;
+
+	@Value("${spring.mail.username}")
+	private String username;
+
+	@Value("${spring.mail.password}")
+	private String password;
+
+	@Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+	private String startTslEnable;
+
+	@Value("${spring.mail.properties.mail.smtp.starttls.required}")
+	private String startTslRequired;
+
+	@Value("${spring.mail.properties.mail.smtp.auth}")
+	private String auth;
+
+	@Bean
+	public JavaMailSender javaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+		mailSender.setDefaultEncoding("UTF-8");
+		mailSender.setJavaMailProperties(getMailProperties());
+
+		return mailSender;
+	}
+
+	private Properties getMailProperties() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", auth);
+		properties.put("mail.smtp.starttls.enable", startTslEnable);
+		properties.put("mail.smtp.starttls.required", startTslRequired);
+
+		return properties;
+	}
+}
---
+++ src/main/java/project/tosstock/common/error/exception/EmailException.java
@@ -0,0 +1,7 @@
+package project.tosstock.common.error.exception;
+
+public class EmailException extends RuntimeException {
+	public EmailException(String message) {
+		super(message);
+	}
+}
