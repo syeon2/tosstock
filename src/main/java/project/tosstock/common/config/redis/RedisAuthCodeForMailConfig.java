3cefa1f193e1d98883d1e7f9197eae7319e7e2ab
SUYEON KIM <waterkite94@gmail.com>
2024-05-24 11:29:12 +0900

    [CHORE] add redis config and test setting
    - Add: Redis config for auth code
    - Add: test container for redis setting

---
+++ src/main/java/project/tosstock/common/config/redis/RedisAuthCodeForMailConfig.java
@@ -0,0 +1,38 @@
+package project.tosstock.common.config.redis;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisAuthCodeForMailConfig {
+
+	@Value("${spring.data.redis_mail.host}")
+	private String host;
+
+	@Value("${spring.data.redis_mail.port}")
+	private Integer port;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(host, port);
+	}
+
+	@Bean
+	@Qualifier(value = "redisAuthCodeForMailTemplate")
+	public RedisTemplate<String, String> redisAuthCodeForMailTemplate() {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+		return redisTemplate;
+	}
+}
