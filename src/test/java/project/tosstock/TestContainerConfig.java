199408d422e686a0bfeb1f7f0b60a53cbb72bad9
SUYEON KIM <waterkite94@gmail.com>
2024-05-24 18:57:23 +0900

    [FIX] fix occur build error

--- src/test/java/project/tosstock/TestContainerConfig.java
+++ src/test/java/project/tosstock/TestContainerConfig.java
@@ -9,20 +9,20 @@ public class TestContainerConfig implements BeforeAllCallback {
 
 	private static final String REDIS_AUTH_CODE_IMAGE = "redis:latest";
 	private static final int REDIS_AUTH_CODE_PORT = 6379;
-	private GenericContainer REDIS_AUTH_CODE;
+	private GenericContainer redisForAuthCode;
 
 	@Override
 	public void beforeAll(ExtensionContext context) {
 		/*
 		 * Redis Auth Code To Join Member
 		 */
-		REDIS_AUTH_CODE = new GenericContainer(DockerImageName.parse(REDIS_AUTH_CODE_IMAGE))
+		redisForAuthCode = new GenericContainer(DockerImageName.parse(REDIS_AUTH_CODE_IMAGE))
 			.withExposedPorts(REDIS_AUTH_CODE_PORT);
-		REDIS_AUTH_CODE.start();
+		redisForAuthCode.start();
 
-		System.setProperty("spring.data.redis_mail.host", REDIS_AUTH_CODE.getHost());
+		System.setProperty("spring.data.redis_mail.host", redisForAuthCode.getHost());
 		System.setProperty("spring.data.redis_mail.port",
-			String.valueOf(REDIS_AUTH_CODE.getMappedPort(REDIS_AUTH_CODE_PORT
+			String.valueOf(redisForAuthCode.getMappedPort(REDIS_AUTH_CODE_PORT
 			)));
 	}
 }
